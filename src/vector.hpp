54a8490286e765dc421dd8631549e9146c144356
BoskiJanusz <patmich@onet.eu>
2023-10-27 15:26:26 +0200

    class skeleton

--- src/vector.hpp
+++ src/vector.hpp
@@ -6,9 +6,19 @@ template<class T>
 class vector
 {
 private:
-    /* data */
+    T* ptr_ = nullptr;
+    size_t acctualSize_ = 0;
+    size_t capacity = 2;
 public:
     vector(){};
-    ~vector(){};
+    vector(const vector &obj){}; // copy constructor
+    vector(vector &&obj){}; // move constructor
+    vector& operator=(const vector &obj){}; // copy assignment operator
+    vector& operator=(vector &&obj){};// move assignment operator
+    T& operator[](const size_t& index){};
+    T& at(const size_t& index){};
+    ~vector(){}; //destructor
+    void push_back(const T& obj){}; //push_back function
+
 };
 } // namespace my
