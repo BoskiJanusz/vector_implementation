43fa962afd97df6995718ccda13ec533693070e9
BoskiJanusz <patmich@onet.eu>
2023-10-30 18:29:24 +0100

    new test for push_back method with three elements

--- test/vector_tests.cpp
+++ test/vector_tests.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "../src/vector.hpp"
 
-TEST(VectorTest, IntValueIntoVectorTest){
+TEST(VectorTest, IntOneValueIntoVectorTest){
     my::vector<int> v{};
     int a = 5;
     v.push_back(a);
@@ -11,7 +11,18 @@ TEST(VectorTest, IntValueIntoVectorTest){
     EXPECT_EQ(value, expected);
 }
 
-TEST(VectorTest, CharValueIntoVectorTest){
+TEST(VectorTest, IntThreeValueIntoVectorTest){
+    my::vector<int> v{};
+    int a = 5;
+    v.push_back(a);
+    v.push_back(a);
+    v.push_back(a);
+    auto value = v[2];
+    auto expected = 5;
+    EXPECT_EQ(value, expected);
+}
+
+TEST(VectorTest, CharOneValueIntoVectorTest){
     my::vector<char> v{};
     char c = 't';
     v.push_back(c);
@@ -20,7 +31,18 @@ TEST(VectorTest, CharValueIntoVectorTest){
     EXPECT_EQ(value, expected);
 }
 
-TEST(VectorTest, StringValueIntoVectorTest){
+TEST(VectorTest, CharThreeValueIntoVectorTest){
+    my::vector<char> v{};
+    char c = 't';
+    v.push_back(c);
+    v.push_back(c);
+    v.push_back(c);
+    auto value = v[2];
+    auto expected = 't';
+    EXPECT_EQ(value, expected);
+}
+
+TEST(VectorTest, StringOneValueIntoVectorTest){
     my::vector<std::string> v{};
     std::string str{"test"};
     v.push_back(str);
@@ -28,4 +50,17 @@ TEST(VectorTest, StringValueIntoVectorTest){
     auto expected = "test";
     EXPECT_EQ(value, expected);
 }
+
+TEST(VectorTest, StringThreeValueIntoVectorTest){
+    my::vector<std::string> v{};
+    std::string str{"test"};
+    v.push_back(str);
+    v.push_back(str);
+    v.push_back(str);
+    auto value = v[2];
+    auto expected = "test";
+    EXPECT_EQ(value, expected);
+}
+
+
 
