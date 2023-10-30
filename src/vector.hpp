3ac508db934a77ecf2d9c4d8861d0264370abf09
BoskiJanusz <patmich@onet.eu>
2023-10-30 18:03:46 +0100

    implementations of: 	operator[] 	push_back() 	default constructor

--- src/vector.hpp
+++ src/vector.hpp
@@ -10,15 +10,22 @@ private:
     size_t acctualSize_ = 0;
     size_t capacity = 2;
 public:
-    vector(){};
+    vector(){
+        ptr_ = new T[capacity];
+    };
     vector(const vector &obj){}; // copy constructor
     vector(vector &&obj){}; // move constructor
     vector& operator=(const vector &obj){}; // copy assignment operator
     vector& operator=(vector &&obj){};// move assignment operator
-    T& operator[](const size_t& index){};
-    T& at(const size_t& index){};
     ~vector(){}; //destructor
-    void push_back(const T& obj){}; //push_back function
-
+    T& operator[](const size_t& index){
+        return ptr_[index];
+    };
+    T& at(const size_t& index){};
+    T& front(){};
+    T& back(){};
+    void push_back(const T& obj){
+        ptr_[acctualSize_] = obj;
+    }; //push_back function
 };
 } // namespace my
