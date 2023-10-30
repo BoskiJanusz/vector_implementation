bb660f41bf87d27481a6714cdadf290c38c578d8
BoskiJanusz <patmich@onet.eu>
2023-10-30 16:55:18 +0100

    input vector tests

--- test/vector_tests.cpp
+++ test/vector_tests.cpp
@@ -1,9 +1,31 @@
 #include <gtest/gtest.h>
+#include <string>
+#include "../src/vector.hpp"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(VectorTest, IntValueIntoVectorTest){
+    my::vector<int> v{};
+    int a = 5;
+    v.push_back(a);
+    auto value = v[0];
+    auto expected = 5;
+    EXPECT_EQ(value, expected);
 }
+
+TEST(VectorTest, CharValueIntoVectorTest){
+    my::vector<char> v{};
+    char c = 't';
+    v.push_back(c);
+    auto value = v[0];
+    auto expected = 't';
+    EXPECT_EQ(value, expected);
+}
+
+TEST(VectorTest, StringValueIntoVectorTest){
+    my::vector<std::string> v{};
+    std::string str{"test"};
+    v.push_back(str);
+    auto value = v[0];
+    auto expected = "test";
+    EXPECT_EQ(value, expected);
+}
+
