5e2c1468b07575d6d16dc4d34b14de76609d71e9
BoskiJanusz <patmich@onet.eu>
2023-11-01 15:37:26 +0100

    implrementation for copy constructor and constructor with T argument

--- src/vector.hpp
+++ src/vector.hpp
@@ -10,12 +10,25 @@ class vector
 private:
     T* ptr_ = nullptr;
     size_t acctualSize_ = 0;
-    size_t capacity = 2;
+    size_t capacity_ = 2;
 public:
     vector(){
-        ptr_ = new T[capacity];
+        ptr_ = new T[capacity_];
     };
-    vector(const vector &obj){}; // copy constructor
+
+    vector(const T &obj){
+        ptr_ = new T[capacity_];
+        ptr_[0] = obj;
+        acctualSize_ ++;
+    }
+    vector(const vector &obj){
+        acctualSize_ = obj.acctualSize_;
+        capacity_ = obj.capacity_;
+        ptr_ = new T[capacity_];
+        for(size_t i = 0; i < acctualSize_; ++i){
+            ptr_[i] = obj.ptr_[i];
+        }
+    }; // copy constructor
     vector(vector &&obj){}; // move constructor
     vector& operator=(const vector &obj){}; // copy assignment operator
     vector& operator=(vector &&obj){};// move assignment operator
@@ -29,9 +42,9 @@ public:
     T& front(){};
     T& back(){};
     void push_back(const T& obj){
-        if(acctualSize_ == capacity){
-            capacity *= 2;
-            auto tempPtr = new T[capacity];
+        if(acctualSize_ == capacity_){
+            capacity_ *= 2;
+            auto tempPtr = new T[capacity_];
             for(size_t i = 0; i < acctualSize_; ++i){
                 tempPtr[i] = ptr_[i];
             }
