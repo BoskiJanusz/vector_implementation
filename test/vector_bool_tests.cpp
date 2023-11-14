1256d8425719e8e3b29fc8502b34550ba6b1bd0e
BoskiJanusz <patmich@onet.eu>
2023-11-14 18:04:51 +0100

    new first test for vector<bool> for push_back

---
+++ test/vector_bool_tests.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <string>
+#include "../src/vector.hpp"
+
+TEST(VectorBoolTest, OneValueIntoVectorTest){
+    my::vector<bool> v{};
+    bool a = 1;
+    v.push_back(a);
+    auto value = v[0];
+    std::cout << v[0];
+    auto expected = true;
+    EXPECT_EQ(value, expected);
+}
