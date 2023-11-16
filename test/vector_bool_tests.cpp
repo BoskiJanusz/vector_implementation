93286f1e2fd10b9513d4e9a161b7ff24fcec4211
BoskiJanusz <patmich@onet.eu>
2023-11-16 16:05:53 +0100

    test for reference of a bit

--- test/vector_bool_tests.cpp
+++ test/vector_bool_tests.cpp
@@ -31,3 +31,71 @@ TEST(VectorBoolTest, FourTrueValuesIntoVectorTest) {
     auto expected = true;
     EXPECT_EQ(value, expected);
 }
+
+TEST(VectorBoolTest, FourTrueValuesIntoVectorFifthIsEqualZeroTest) {
+    my::vector<bool> v{};
+    bool a = 1;
+    v.push_back(a);
+    v.push_back(a);
+    v.push_back(a);
+    v.push_back(a);
+    auto value = v[4];
+    auto expected = false;
+    EXPECT_EQ(value, expected);
+}
+
+TEST(VectorBoolTest, FillOneTableWithValuesLastBitIsEqualOneTest) {
+    my::vector<bool> v{};
+    bool a = 1;
+    for(size_t i = 0; i < 64; i++){
+        v.push_back(a);
+    }
+    auto value = v[63];
+    auto expected = true;
+    EXPECT_EQ(value, expected);
+}
+
+TEST(VectorBoolTest, ArrayOfTwoElementsSecondBitOfFirstElementIsEqualOneTest) {
+    my::vector<bool> v{};
+    bool a = 1;
+    for(size_t i = 0; i < 65; i++){
+        v.push_back(a);
+    }
+    auto value = v[64];
+    auto expected = true;
+    EXPECT_EQ(value, expected);
+}
+
+TEST(VectorBoolTest, ArrayOfTwoElementsSecondBitOfSecondElementIsEqualZeroTest) {
+    my::vector<bool> v{};
+    bool a = 1;
+    for(size_t i = 0; i < 65; i++){
+        v.push_back(a);
+    }
+    auto value = v[65];
+    auto expected = false;
+    EXPECT_EQ(value, expected);
+}
+
+TEST(VectorBoolTest, ReferenceOfABitVectorTest) {
+    my::vector<bool> v{};
+    bool a = 1;
+    v.push_back(a);
+    v[0] = 0;
+    auto value = v[0];
+    auto expected = false;
+    EXPECT_EQ(value, expected);
+}
+
+TEST(VectorBoolTest, ReferenceOfAFourBitsVectorTest) {
+    my::vector<bool> v{};
+    bool a = 1;
+    v.push_back(a);
+    v.push_back(a);
+    v.push_back(a);
+    v.push_back(a);
+    v[3] = 0;
+    auto value = v[3];
+    auto expected = false;
+    EXPECT_EQ(value, expected);
+}
