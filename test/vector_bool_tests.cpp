460f04b57e7886b004d7e9e4e0e82adbf5d318e5
BoskiJanusz <patmich@onet.eu>
2023-11-16 17:13:01 +0100

    Tets for method at

--- test/vector_bool_tests.cpp
+++ test/vector_bool_tests.cpp
@@ -152,3 +152,40 @@ TEST(VectorBoolTest, OneVectorValueMovedToAnotherVectorMoveAssignmentOperatorTes
     auto expected = true;
     EXPECT_EQ(value, expected);
 }
+
+TEST(VectorBoolTest, MethodAtOneTrueValueIntoVectorTest) {
+    my::vector<bool> v{};
+    bool a = 1;
+    v.push_back(a);
+    auto value = v.at(0);
+    auto expected = true;
+    EXPECT_EQ(value, expected);
+}
+
+TEST(VectorBoolTest, MethodOneFalseValueIntoVectorTest) {
+    my::vector<bool> v{};
+    bool a = 0;
+    v.push_back(a);
+    auto value = v.at(0);
+    auto expected = false;
+    EXPECT_EQ(value, expected);
+}
+
+TEST(VectorBoolTest, MethodFourTrueValuesIntoVectorTest) {
+    my::vector<bool> v{};
+    bool a = 1;
+    v.push_back(a);
+    v.push_back(a);
+    v.push_back(a);
+    v.push_back(a);
+    auto value = v.at(3);
+    auto expected = true;
+    EXPECT_EQ(value, expected);
+}
+
+TEST(VectorBoolTest, OutOfRangeVectorTest) {
+    my::vector<bool> v{};
+    bool a = 0;
+    v.push_back(a);
+    ASSERT_THROW(v.at(1), std::out_of_range);
+}
